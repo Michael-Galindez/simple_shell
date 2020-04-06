90375be25c301e7d3a7051381fef499902f89526
acamilojuan <1600@holbertonschool.com>
2020-04-06 14:30:13 +0000

    startsh.c file modified

--- startsh.c
+++ startsh.c
@@ -1,29 +1,25 @@
 #include "shell.h"
 /**
  * main - function thattakes commands from the user, stdin.
- * @argc: arguments.
- * @argv: arguments.
- * Return: Always  zero.
+ * @argc: Arguments to use.
+ * @argv: Double pointer to the array of arguments.
+ * Return: Always zero.
  */
 int main(int argc, char **argv)
 {
-  (void) argc, (void) argv;
-  char *l;
-  int checker;
-  char **args;
+	(void) argc, (void) argv;
+	char *l, **args;
+	int checker;
 
-
-do {
-    /** Prints Prompt To User */
-    printf("SH>$ ");
-    /** reads the line */
-    l = readline();
-    /** Separates the line to take and delims commands */
-    args = separate(l);
-    /** Execute the commands given by the user */
-    checker = execute(args);
-}
-/** while executes args */
-while (checker);
-return (0);
+	do {
+		/** Prints Prompt To User */
+		printf("SH>$ ");
+		/** reads the line */
+		l = readline();
+		/** Separates the line to take and delims commands */
+		args = separate(l);
+		/** Execute the commands given by the user */
+		checker = execute(args);
+	} while (checker);
+	return (0);
 }
