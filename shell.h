f306bbc679c3c860b4976ac2361439995cb1d4aa
Michael-Galindez <michaelgalindez274@gmail.com>
2020-04-06 17:59:24 -0500

    adding header file for the project

---
+++ shell.h
@@ -0,0 +1,32 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+/**
+ * struct command_t - pointers to builtins.
+ * @name: name of the command.
+ * @s: pointer to command.
+ */
+typedef struct command_t
+{
+  char *name;
+  int (*s)();
+
+} command_t;
+
+char *readline(void);
+char **separate(char *l);
+int execute(char **args);
+int run(char **args);
+int __exit(void);
+int help(void);
+int cd(char **args);
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+
+#endif
