46646602108a38498177a250859ca19733085688
Michael-Galindez <michaelgalindez274@gmail.com>
2020-04-07 14:10:04 -0500

    fixing header file

--- shell.h
+++ shell.h
@@ -1,13 +1,11 @@
 #ifndef SHELL_H
 #define SHELL_H
 #define N_COMMANDS(x, y) (sizeof(x) / sizeof(y))
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
-
 /**
  * struct command_t - pointers to builtins.
  * @name: name of the command.
