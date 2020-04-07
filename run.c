d1109869627732eebbe751020b53fab4e2208f16
Michael-Galindez <michaelgalindez274@gmail.com>
2020-04-06 19:28:25 -0500

    finished run function (no indent)

--- run.c
+++ run.c
@@ -1,28 +1,31 @@
 #include "shell.h"
 /**
-* run - Function to run the child processes.
-* @args: Double pointer to the list of arguments.
-* Return:
-*/
-int run(chr **args)
+ * run - runs the program with fork.
+ * @args: arguments.
+ * Return: always 1.
+ */
+int run(char **args)
 {
-	pid_t pid;
-	int status;
-	
-	pid = fork();
-	if (pid == 0)
-	{
-		if (execve(args[0], args) == -1)
-			perror(">$: execve failure");
-		exit(EXIT_FAILURE);
-	}
-	else if (pid < 0)
-		perror(">$: execve failure)");
-	else
-	{
-		do {
-			wpid = waitpid();
-		} while ();
-	}
-	return (0);
+int status;
+pid_t pid;
+
+/** child process */
+pid = fork();
+if (pid == 0)
+{
+if (execvp(args[0], args) == -1)
+perror("execvp failed");
+exit(EXIT_FAILURE);
+}
+else if (pid < 0)
+/**  if error forking */
+perror(">$ ");
+else
+/** Parent process */
+{
+do {
+waitpid(pid, &status, WUNTRACED);
+} while (!WIFEXITED(status) && !WIFSIGNALED(status));
+}
+return (1);
 }
