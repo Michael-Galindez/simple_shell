78c3f9bbbb3b6c22e79a18c778ca76185378cdad
acamilojuan <1600@holbertonschool.com>
2020-04-07 17:16:40 +0000

    run.c file modified

--- run.c
+++ run.c
@@ -1,31 +1,33 @@
 #include "shell.h"
 /**
- * run - runs the program with fork.
- * @args: arguments.
- * Return: always 1.
+ * run - Function that runs the program with fork.
+ * @args: Double pointer to the arguments.
+ * Return: Always 1.
  */
 int run(char **args)
 {
-int status;
-pid_t pid;
+	int status;
+	pid_t pid;
 
-/** child process */
-pid = fork();
-if (pid == 0)
-{
-if (execvp(args[0], args) == -1)
-perror("execvp failed");
-exit(EXIT_FAILURE);
-}
-else if (pid < 0)
-/**  if error forking */
-perror(">$ ");
-else
-/** Parent process */
-{
-do {
-waitpid(pid, &status, WUNTRACED);
-} while (!WIFEXITED(status) && !WIFSIGNALED(status));
-}
-return (1);
+	/** child process */
+	pid = fork();
+	if (pid == 0)
+	{
+		if (execvp(args[0], args) == -1)
+		{
+			perror("execvp failed");
+			exit(EXIT_FAILURE);
+		}
+	}
+	else if (pid < 0)
+	/**  if error forking */
+		perror(">$ ");
+	else
+	/** Parent process */
+	{
+		do {
+			waitpid(pid, &status, WUNTRACED);
+		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
+	}
+	return (1);
 }
