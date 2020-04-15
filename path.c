bea69df361e82ea0a44c37d48ac7d1efe0de3ec2
acamilojuan <1600@holbertonschool.com>
2020-04-15 23:19:05 +0000

    path.c betty fixed

--- path.c
+++ path.c
@@ -6,22 +6,21 @@
  */
 char *pathvalue(char **env)
 {
-char *token;
-int i = 0;
-char *temp;
+	char *token, *temp;
+	int i = 0;
 
-while (env[i])
-{
-temp = env[i];
-token = strtok(temp, "=");
-if (_strcmp(token, "PATH") == 0)
-{
-token = strtok(NULL, "=");
-return (token);
-}
-i++;
-}
-return (NULL);
+	while (env[i])
+	{
+		temp = env[i];
+		token = strtok(temp, "=");
+		if (_strcmp(token, "PATH") == 0)
+		{
+			token = strtok(NULL, "=");
+			return (token);
+		}
+		i++;
+	}
+	return (NULL);
 }
 /**
  * _strstr -  a function that locates a substring..
@@ -31,23 +30,22 @@ return (NULL);
  */
 char *_strstr(char *haystack, char *needle)
 {
-int i;
-int  j = 0;
+	int i, j = 0;
 
-if (needle[j] == '\0')
-return (haystack);
-for (i = 0; haystack[i] != '\0'; i++)
-{
-if (haystack[i] == needle[0])
-{
-for (j = 0; needle[j] != '\0' && haystack[i + j]
-&& needle[j] == haystack[i + j]; j++)
-;
-if (needle[j] == 0)
-return (haystack + i);
-}
-}
-return (0);
+	if (needle[j] == '\0')
+		return (haystack);
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		if (haystack[i] == needle[0])
+		{
+		for (j = 0; needle[j] != '\0' && haystack[i + j]
+		&& needle[j] == haystack[i + j]; j++)
+			;
+		if (needle[j] == 0)
+			return (haystack + i);
+		}
+	}
+	return (0);
 }
 /**
  * pathch - tokenize path and check if the buffer exists.
@@ -57,33 +55,30 @@ return (0);
  */
 char *pathch(char *token, char **env)
 {
-static char buffer[1024] = {0};
-char *path;
-char *tok;
-struct stat st;
-char *a, *b;
+	static char buffer[1024] = {0};
+	char *path, *a, *b, *tok;
+	struct stat st;
 
-path = pathvalue(env);
-if (!path)
-path = "";
-a = _strstr(path, "::");
-b = _strstr(path, ":/bin");
-if (path[0] == ':' || (a && (a < b)))
-{
-if (stat(token, &st) == 0)
-return (token);
-}
-
-tok = strtok(path, ":");
-while (tok)
-{
-_strcat(buffer, tok);
-_strcat(buffer, "/");
-_strcat(buffer, token);
-if (stat(buffer, &st) == 0)
-return (buffer);
-tok = strtok(NULL, ":");
-_memset(buffer, 0, 1024);
-}
-return (token);
+	path = pathvalue(env);
+	if (!path)
+		path = "";
+	a = _strstr(path, "::");
+	b = _strstr(path, ":/bin");
+	if (path[0] == ':' || (a && (a < b)))
+	{
+		if (stat(token, &st) == 0)
+		return (token);
+	}
+	tok = strtok(path, ":");
+	while (tok)
+	{
+		_strcat(buffer, tok);
+		_strcat(buffer, "/");
+		_strcat(buffer, token);
+		if (stat(buffer, &st) == 0)
+			return (buffer);
+		tok = strtok(NULL, ":");
+		_memset(buffer, 0, 1024);
+	}
+	return (token);
 }
